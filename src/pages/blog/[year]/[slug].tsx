71716141dec4d74e2a9fe97ca939186e7d484c1b
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-21 22:48:09 +0900

    add

--- src/pages/blog/[year]/[slug].tsx
+++ src/pages/blog/[year]/[slug].tsx
@@ -2,20 +2,21 @@ import { GetServerSidePropsContext } from "next"
 import React from "react"
 import { Helmet } from "react-helmet"
 
-const redirectTo = (slug: string) => `https://blog.terrier.dev/blog/${slug}`
+const redirectTo = (year: string, slug: string) => `https://blog.terrier.dev/blog/${year}/${slug}`
 export const getServerSideProps = async ({ res, params }: GetServerSidePropsContext) => {
-  const slug = params?.slug
-  if (typeof slug !== "string") {
+  const { year, slug } = params || {}
+  if (typeof slug !== "string" || typeof year !== "string") {
     res.statusCode = 404
     res.end()
     return
   }
-  res.setHeader('Location', redirectTo(slug))
+  res.setHeader('Location', redirectTo(year, slug))
   res.statusCode = 302
   res.end()
+  return { year, slug }
 }
-export default function Blog({ slug }: any) {
-  const url = redirectTo(slug)
+export default function Blog({ year, slug }: any) {
+  const url = redirectTo(year, slug)
   return <div>
     <Helmet>
 
