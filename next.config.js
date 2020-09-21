19e426280092b586248279cfb811f5860c4be19c
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-21 22:56:07 +0900

    add next config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ const withImages = require('next-images')
 module.exports = withImages({
   webpack(config, options) {
     return config
+  },
+  async redirects() {
+    return [
+      {
+        source: '/blog/:year/:slug', // automatically becomes /docs/with-basePath
+        destination: 'https://blog.terrier.dev/blog/', // automatically becomes /docs/another
+        permanent: false,
+      }
+    ]
   }
 })
