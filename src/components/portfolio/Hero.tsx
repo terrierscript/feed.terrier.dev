4c7b1a31bb4f868155a6e4b07012f45bab55ef1f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-21 23:38:08 +0900

    deploy

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/core"
 import React from "react"
 import { Dog } from "../layout/header/Dog"
 
-
 export const Hero = () => {
   return <Box px={20}>
     <Dog size={"100%"} />
