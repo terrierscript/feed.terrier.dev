b52c9b2831efe7e1f60a4740ce1921f045219dd7
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-23 13:26:53 +0900

    filter

--- src/lib/feed/parser/parseScrapbox.ts
+++ src/lib/feed/parser/parseScrapbox.ts
@@ -5,12 +5,12 @@ import { FeedItem } from "../Item"
 export const parseScrapbox = async (url: string): Promise<FeedItem[]> => {
   // @ts-ignore
   const { data } = await axios.get(url)
-  return data.pages.map(({ title,descriptions, created, ...p }: any) => {
+  return data.pages.map(({ title, descriptions, created, ...p }: any): FeedItem => {
     return {
       title: title,
       date: new Date(created * 1000),
       description: descriptions.join("\n"),
       link: `https://scrapbox.io/terrierscript/${encodeURIComponent(title)}`
     }
-  })
+  }).filter((item: { description: string | any[] }) => item.description.length > 0)
 }
