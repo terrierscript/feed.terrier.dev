73fe8370ddd9b17bd64bddbbd3fda05d7e6eae5e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-28 22:25:42 +0900

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
 
+
+import { CacheProvider } from '@emotion/react'
+// import { cache } from '@emotion/css'
+
 import { CSSReset, Grid, ThemeProvider } from "@chakra-ui/core"
 import React from "react"
 import { Layout } from "../components/layout/Layout"
@@ -7,12 +11,14 @@ function MyApp({
   Component,
   pageProps
 }: any) {
+  // return <CacheProvider value={cache}>
   return <ThemeProvider>
     <CSSReset />
     <Layout>
       <Component {...pageProps} />
     </Layout>
   </ThemeProvider>
+  // </CacheProvider>
 }
 
 export default MyApp
