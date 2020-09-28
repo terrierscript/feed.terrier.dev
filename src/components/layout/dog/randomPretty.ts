cd375d2abce41d5807d5cbfc67f52a944f5af75f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-28 23:25:06 +0900

    add

--- src/components/layout/dog/randomPretty.ts
+++ src/components/layout/dog/randomPretty.ts
@@ -1,19 +1,19 @@
 // @ts-nocheck
 
 const imgs = [
-  require("./pretty1.jpg"),
-  require("./pretty1.jpg"),
-  require("./pretty1.jpg"),
-  require("./pretty1.jpg"),
-  require("./pretty1.jpg"),
-  require("./pretty1.jpg"),
-  require("./pretty1.jpg"),
-  require("./pretty2.jpg"),
-  require("./pretty3.jpg"),
-  require("./pretty4.jpg"),
-  require("./pretty6.jpg"),
-  require("./pretty7.jpg"),
-  require("./pretty8.jpg"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty1.jpg?resize&size=600&webp"),
+  require("./pretty2.jpg?resize&size=600&webp"),
+  require("./pretty3.jpg?resize&size=600&webp"),
+  require("./pretty4.jpg?resize&size=600&webp"),
+  require("./pretty6.jpg?resize&size=600&webp"),
+  require("./pretty7.jpg?resize&size=600&webp"),
+  require("./pretty8.jpg?resize&size=600&webp"),
 ]
 export const randomPretty = () => {
   return imgs[Math.floor(Math.random() * imgs.length)]
