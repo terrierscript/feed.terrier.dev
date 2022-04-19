643850a24935a540446e55e4e3008d4657567616
terrierscript <13282103+terrierscript@users.noreply.github.com>
2022-04-20 08:22:16 +0900

    sort feed

--- src/lib/feed/parser/parseMedia.ts
+++ src/lib/feed/parser/parseMedia.ts
@@ -5,14 +5,15 @@ import { parseZennRss } from "./parseZennRss"
 import { parseZennScrap } from "./parseZennScrap"
 
 
-export const parseMedia = (config: Config) => {
+export const parseMedia = async (config: Config) => {
   switch (config.id) {
     case "scrapbox":
       return parseScrapbox(config.origin)
     case "zenn":
       return parseZennRss(config.origin)
     case "zennScrap":
-      return parseZennScrap(config.origin)
+      const scr = await parseZennScrap(config.origin)
+      return scr
     default:
       return parseRss(config.origin)
   }
