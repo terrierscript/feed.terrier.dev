8738495198939e246d80bb7a65cdb110489abf11
S. Suzuki <terrierscript@gmail.com>
2025-03-15 00:27:01 +0900

    top page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,25 @@
 
 import React from "react"
 import { Layout } from "../components/layout/Layout"
-import { ChakraProvider, StylesProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MantineProvider } from '@mantine/core'
 import { customTheme } from "../lib/theme"
+import { mantineTheme } from "../lib/mantineTheme"
+import '@mantine/core/styles.css'
 
 function MyApp({
   Component,
   pageProps
 }: any) {
-  // <CacheProvider value={cache}>
-  return <ChakraProvider
-    theme={customTheme}
-  >
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </ChakraProvider>
-  // </CacheProvider>
+  return (
+    <ChakraProvider theme={customTheme}>
+      <MantineProvider theme={mantineTheme} defaultColorScheme="auto">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,20 @@
 import { Box } from '@chakra-ui/react'
-import Head from 'next/head'
 import React from 'react'
-import { Feeds } from "../components/feeds/Feed"
+import { Feeds } from "../components/mantine/feeds/Feed"
 import { Portfolio } from '../components/portfolio/Portfolio'
 import { loadAllForSSR } from '../lib/feed/loadAll'
 
-// @ts-ignore
-export function reportWebVitals(metric) {
-  console.log(metric)
-}
-
-export async function getStaticProps() {
+export async function getServerSideProps() {
   try {
-    const random = Math.random()
     const feeds = await loadAllForSSR()
-    // console.log(feeds)
-    return { props: { feeds, random }, revalidate: 60 }
+    return { props: { feeds } }
   } catch (e) {
     console.error(e)
-    return { props: { feeds: [] }, revalidate: 60 }
+    return { props: { feeds: [] } }
   }
 }
 
-export default function Home({ feeds, random }: any) {
+export default function Home({ feeds }: { feeds: any[] }) {
   return (
     <Box>
       <Portfolio />
