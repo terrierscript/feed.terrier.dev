501b09fcc7cde887c8b3870a2b516f747d619e5c
S. Suzuki <terrierscript@gmail.com>
2025-03-15 13:17:33 +0900

    remove pages

--- src/pages/api/p/feeds.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { loadAllForSSR } from "../../../lib/feed/loadAll"
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const feeds = await loadAllForSSR()
-  res.json(feeds)
-}
